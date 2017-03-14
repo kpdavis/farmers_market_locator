4876fddfd72e86017e3aeaeed388b4f77686dad3
Keith Davis <kpdavis.kd@outlook.com>
2017-03-13 20:09:17 -0600

    Have api working and data print out of markets with a clear field so it refreshes page each search

--- public/js/locator.js
+++ public/js/locator.js
@@ -1,73 +1,41 @@
-// function setMap() {
-//   var uluru = {lat: -25.363, lng: 131.044};
-//   var map = new google.maps.Map(document.getElementById('map'), {
-//     zoom: 4,
-//     center: uluru
-//   });
-//   var marker = new google.maps.Marker({
-//     position: uluru,
-//     map: map
-//   });
-// }
-//
-// function initMap() {
-//   var map = new google.maps.Map(document.getElementById('map'), {
-//     center: {lat: -34.397, lng: 150.644},
-//     zoom: 6
-//   });
-//   var infoWindow = new google.maps.InfoWindow({map: map});
-//   // Try HTML5 geolocation.
-//   if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition(function(position) {
-//       var pos = {
-//         lat: position.coords.latitude,
-//         lng: position.coords.longitude
-//       };
-//
-//     infoWindow.setPosition(pos);
-//     infoWindow.setContent('Location found.');
-//     map.setCenter(pos);
-//     }, function() {
-//       handleLocationError(true, infoWindow, map.getCenter());
-//     });
-//     } else {
-//     // Browser doesn't support Geolocation
-//       handleLocationError(false, infoWindow, map.getCenter());
-//     }
-//   }
-//
-//   function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-//     infoWindow.setPosition(pos);
-//     infoWindow.setContent(browserHasGeolocation ?
-//                               'Error: The Geolocation service failed.' :
-//                               'Error: Your browser doesn\'t support geolocation.');
-// }
-$(document).ready(function() {
-  getMarket(areaZip());
-})
-var areaZip= function() {
+
+function areaZip() {
+  $('form').submit(function(e) {
+
+    e.preventDefault()
+  })
   $('#search').keypress(function(e) {
     if (e.keyCode === 13) {
-      return $('#search').val()
-    }
+      $('.marketInfo').empty();
+      var zip = $('#search').val()
+      getMarket(zip);
+   }
   });
 }
 function getMarket(zip) {
-  var resulsStr= ""
-  var id=""
-  var name= ""
-   var markets = $.get("http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=" + zip);
-   for (var i = 0; i < markets.length; i++) {
-      id = markets[i].id;
-      name = markets[i].marketname;
-      resulsStr += "<tr>" + "\n<td data-field=\"id\">" + name + "</td>\n";
+  var resultsStr= ""
+  var markets = $.get("http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=" + zip).then(function(markets) {
+     var data = markets.results;
 
-      var data = $.get("http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=" +id);
-      address = data.Address;
-      products = data.Products;
-      schedule = data.Schedule;
-      resulsStr += "<td data-field=\"address\">" + address + "</td>\n<td data-field=\"schedule\">" + schedule + "</td>\n<td data-field=\"products\"" + products + "</td>\n</tr>";
-      $('.marketInfo').append(resulsStr);
-  }
-
+     for (var i = 0; i < data.length; i++) {
+       resultsStr = "\n<tr class=\"FM" + i + "\">\n<td data-field=\"id\">" + data[i].marketname + "</td>\n"
+       $(".marketInfo").append(resultsStr)
+       getInfo(data[i].id, i)
+      }
+  });
 }
+
+function getInfo(id, i) {
+
+  var info = $.get("http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=" + id).then(function(info) {
+    address = info.marketdetails.Address;
+    //products = info.marketdetails.Products;
+    //schedule = info.marketdetails.Schedule;
+    var str = "<td data-field=\"address\">" + address + "</td>\n</tr>"
+    //data-field=\"schedule\">" + schedule + "</td>\n<td data-field=\"products\"" + products + "</td>\n</tr>");
+    console.log(str);
+    $(".FM"+i).append(str)
+  });
+}
+
+areaZip();
