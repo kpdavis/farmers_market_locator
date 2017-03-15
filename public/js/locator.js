b22d339e9c44cc8e74a59976def7f40cc7fbd2ea
Keith Davis <kpdavis.kd@outlook.com>
2017-03-15 10:30:02 -0600

    Refined nav header and color sceam

--- public/js/locator.js
+++ public/js/locator.js
@@ -18,7 +18,7 @@ function getMarket(zip) {
      var data = markets.results;
 
      for (var i = 0; i < data.length; i++) {
-       resultsStr = "\n<tr class=\"FM" + i + "\">\n<td data-field=\"id\">" + data[i].marketname + "</td>\n"
+       resultsStr = "\n<tr class=\"FM" + i + "\">\n<td class=\"name\" data-field=\"id\">" + data[i].marketname + "</td>\n"
        $(".marketInfo").append(resultsStr)
        getInfo(data[i].id, i)
       }
@@ -26,16 +26,20 @@ function getMarket(zip) {
 }
 
 function getInfo(id, i) {
-
   var info = $.get("http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=" + id).then(function(info) {
-    address = info.marketdetails.Address;
-    //products = info.marketdetails.Products;
-    //schedule = info.marketdetails.Schedule;
-    var str = "<td data-field=\"address\">" + address + "</td>\n</tr>"
-    //data-field=\"schedule\">" + schedule + "</td>\n<td data-field=\"products\"" + products + "</td>\n</tr>");
-    console.log(str);
+    var address = info.marketdetails.Address;
+    var map = info.marketdetails.GoogleLink;
+    var str = "<td class=\"address\" data-field=\"address\"><a href=\"" + map + "\">" + address + "</a></td>\n</tr>";
+
     $(".FM"+i).append(str)
   });
 }
+
+function getMap() {
+ $("td").onclick(function() {
+
+ })
+}
+
 
 areaZip();
